951f1ff75b195b3896fe88fb4fc3a45367a708db
Pavel Sheremet <79811436+pavel-sheremet-dev@users.noreply.github.com>
2025-03-02 11:36:46 +0000

    add example with addMatcher

--- src/redux/todosSlice.js
+++ src/redux/todosSlice.js
@@ -1,4 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, isAnyOf } from '@reduxjs/toolkit';
 import { fetchTodos, addTodo, deleteTodo, editTodo } from './todosOperations';
 
 const initialState = {
@@ -16,48 +16,84 @@ const todosSlice = createSlice({
       state.currentTodo = action.payload;
     },
   },
-  extraReducers: builders => {
-    builders
+  extraReducers: builder => {
+    builder
       .addCase(fetchTodos.fulfilled, (state, action) => {
         state.items = action.payload;
-        state.loading = false;
-        state.error = null;
-      })
-      .addCase(fetchTodos.pending, state => {
-        state.loading = true;
-      })
-      .addCase(fetchTodos.rejected, (state, action) => {
-        state.error = action.payload;
       })
       .addCase(addTodo.fulfilled, (state, action) => {
         state.items.push(action.payload);
-        state.loading = false;
-        state.error = null;
-      })
-      .addCase(addTodo.pending, state => {
-        state.loading = true;
-      })
-      .addCase(addTodo.rejected, (state, action) => {
-        state.error = action.payload;
       })
       .addCase(deleteTodo.fulfilled, (state, action) => {
         state.items = state.items.filter(item => item.id !== action.payload);
       })
       .addCase(editTodo.fulfilled, (state, action) => {
-        const index = state.items.findIndex((item) => item.id === action.payload.id);
+        const index = state.items.findIndex(
+          item => item.id === action.payload.id,
+        );
         state.items.splice(index, 1, action.payload);
         state.currentTodo = null;
-        state.loading = false;
       })
-      .addCase(editTodo.pending, state => {
-        state.loading = true;
-      })
-      .addCase(editTodo.rejected, (state, action) => {
-        state.error = action.payload;
-      })
+
+      // .addMatcher(
+      //   action => action.type.endsWith('pending'),
+      //   state => {
+      //     state.loading = true;
+      //     state.error = null;
+      //   },
+      // )
+      // .addMatcher(
+      //   action => action.type.endsWith('fulfilled'),
+      //   state => {
+      //     state.loading = false;
+      //   },
+      // )
+      // .addMatcher(
+      //   action => action.type.endsWith('rejected'),
+      //   (state, action) => {
+      //     state.loading = false;
+      //     state.error = action.payload;
+      //   },
+      // )
+
+      .addMatcher(
+        isAnyOf(
+          fetchTodos.pending,
+          addTodo.pending,
+          deleteTodo.pending,
+          editTodo.pending,
+        ),
+        state => {
+          state.loading = true;
+          state.error = null;
+        },
+      )
+      .addMatcher(
+        isAnyOf(
+          fetchTodos.fulfilled,
+          addTodo.fulfilled,
+          deleteTodo.fulfilled,
+          editTodo.fulfilled,
+        ),
+        state => {
+          state.loading = false;
+        },
+      )
+      .addMatcher(
+        isAnyOf(
+          fetchTodos.rejected,
+          addTodo.rejected,
+          deleteTodo.rejected,
+          editTodo.rejected,
+        ),
+        (state, action) => {
+          state.loading = false;
+          state.error = action.payload;
+        },
+      );
   },
 });
-export const {setCurrentTodo} = todosSlice.actions;
+export const { setCurrentTodo } = todosSlice.actions;
 
 export default todosSlice.reducer;
 
